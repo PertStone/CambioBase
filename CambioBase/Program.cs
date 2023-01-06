08f259d7efeee9ffd5393a4366826347010fd476
Cristian Valero <desarrolladorcontar_08@soportelogico.co>
2023-01-06 17:30:53 -0500

    Agregar archivos de proyecto.

---
+++ CambioBase/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CambioBase
+{
+    class Program
+    {
+        static int tableWidth = 73;
+        public static void Main()
+        {
+            //Parametros de entrada
+            //
+            Console.WriteLine("Ingrese el numero");
+            var sNumero = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ingrese una base entre 2 y 9");
+            var _base = Convert.ToInt32(Console.ReadLine());            
+
+            //Validacion del campo _base
+            //
+            if (_base < 2 || _base > 9)
+            {
+                Console.WriteLine("Los valores ingresados no son correctos por favor intente Nuevamente");
+                Console.WriteLine("Ingrese una base entre 2 y 9");
+                _base = Convert.ToInt32(Console.ReadLine());
+                if (_base < 2 || _base > 9)
+                {
+                    Console.WriteLine("Los valores ingresados no son correctos la aplicacion se cerrara");
+                    Environment.Exit(0);
+                }
+            }
+
+            // creacion de encabezado de la tabla
+            //
+            PrintLine();
+            PrintRow("Operacion", "Cociente", "Residuo");
+
+            // Craecion de variables
+            //
+            var auxiliar = sNumero;
+            var NumeroOriginal = sNumero;
+            var lResiduo = new List<int>();
+            var sGuardaCoeficientes = "";
+
+            while (true)
+            {
+                var sQuociente = auxiliar / _base;
+
+                var sResiduo = auxiliar - (_base * sQuociente);
+                lResiduo.Add(sResiduo);
+                auxiliar = sQuociente;
+
+                PrintLine();
+                PrintRow(sNumero.ToString()+":"+_base,sQuociente.ToString(), sResiduo.ToString());
+                sNumero = auxiliar;
+                if (auxiliar < _base)
+                {                    
+                    lResiduo.Add(auxiliar);
+                    for (var i = 0; i < lResiduo.Count; i++)
+                    {
+                        sGuardaCoeficientes = sGuardaCoeficientes + lResiduo[i].ToString();
+                    }
+                    break;
+                }
+            }
+            Console.WriteLine("\n Por tanto " + NumeroOriginal + " en base 10 = " + Reverse(sGuardaCoeficientes) + " en base " + _base);
+        }
+
+
+        static void PrintLine()
+        {
+            Console.WriteLine(new string('-', tableWidth));
+        }
+
+
+        static void PrintRow(params string[] columns)
+        {
+            int width = (tableWidth - columns.Length) / columns.Length;
+            string row = "|";
+            foreach (string column in columns)
+
+            {
+                row += Aligncenter(column, width) + "|";
+            }
+
+            Console.WriteLine(row);
+
+        }
+
+        static string Aligncenter(string text, int width)
+        {
+            text = text.Length > width ? text.Substring(0, width - 3) + "..." : text;
+
+            if (string.IsNullOrEmpty(text))
+            {
+                return new string(' ', width);
+            }
+
+            else
+            {
+                return text.PadRight(width - (width - text.Length) / 2).PadLeft(width);
+            }
+        }
+
+        static string Reverse(string str)
+        {
+            char[] chars = str.ToCharArray();
+            Array.Reverse(chars);
+            return new string(chars);
+        }
+    }
+}
